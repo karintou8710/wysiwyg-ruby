8a06c1268aa0a344f1a290184e8d5944102ab7b0
karintou8710 <karintou8710@gmail.com>
2026-03-28 19:14:07 +0900

    tiptap editor

---
+++ src/components/ContentEditableEditor.tsx
@@ -0,0 +1,7 @@
+function ContentEditableEditor() {
+  return (
+    <div contentEditable suppressContentEditableWarning spellCheck={false} />
+  )
+}
+
+export default ContentEditableEditor
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,15 +1,26 @@
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import { EditorContent, useEditor } from '@tiptap/react'
 import './Editor.css'
 
 function Editor() {
+  const editor = useEditor({
+    extensions: [Document, Paragraph, Text],
+    content: '<p></p>',
+    immediatelyRender: false,
+    editorProps: {
+      attributes: {
+        class: 'editor',
+        spellcheck: 'false',
+        'aria-label': 'Editor',
+      },
+    },
+  })
+
   return (
-    <div
-      className="editor"
-      contentEditable
-      data-placeholder="Start writing..."
-      suppressContentEditableWarning
-      spellCheck={false}
-    >
-      <p>Text<ruby contentEditable="false">開発<rt>かいはつ</rt></ruby>ルビフル</p>
+    <div className="editor-shell">
+      <EditorContent editor={editor} />
     </div>
   )
 }
